d5efa708e91d3fef957040684f1d76f99695dbef
Justin Fortier <justinffortier@gmail.com>
2018-09-30 06:35:44 -0700

    Added home template

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,14 @@
+<?php
+/**
+ * The template for displaying all products in basic loop
+ *
+ * Template Name: Home Page
+ *
+ * @package Bootstrap
+ */
+ get_header();
+ ?>
 Dope!
+
+<?php
+get_footer();
